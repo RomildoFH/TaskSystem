dd3ada268965801fa184d52e2ab88604984fe139
Romildo <romildo.silvafilho1@gmail.com>
2024-04-24 11:22:38 -0300

    feat: create db context

---
+++ TaskSystem/Data/TasksSystemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskSystem.Models;
+
+namespace TaskSystem.Data
+{
+    public class TasksSystemDbContext : DbContext
+    {
+        public TasksSystemDbContext(DbContextOptions<TasksSystemDbContext> options)
+            : base(options) 
+        {            
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
